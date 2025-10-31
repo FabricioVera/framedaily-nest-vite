d3a13e44993506a211c0b6d021f7d9a38e1a48c9
FabricioVera <fabriciovera7@gmail.com>
2025-10-31 18:12:53 -0300

    feat(build): json-copied-to-build

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/main.ts', 'src/data-source.ts'], // Añadimos data-source al build
+  entry: [
+    'src/main.ts',
+    'src/data-source.ts',
+    'src/database/schema/seed-warframes.ts',
+  ], // Añadimos data-source al build
   splitting: false,
   sourcemap: true,
   clean: true,
   format: ['cjs'],
   target: 'es2021',
   noExternal: ['ts-node', 'tsconfig-paths'], // Importante para TypeORM CLI
+  onSuccess:
+    'cpx "src/database/schema/json/**/*.json" "dist/database/schema/json"',
 });
